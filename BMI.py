383e292a84a47707463c1e7288aa761c0ceaffe8
nyullantyu <nyul101@gmail.com>
2020-05-01 11:40:46 +0200

    asd
    asdasdasd

--- BMI.py
+++ BMI.py
@@ -1,10 +1,11 @@
 olivabogyo = True if input("Kell oliva? i/n") == "i" else False
+#ide is basztam tenni valkamitz
 pepperoni = True if input("Kell pepperoni? i/n") == "i" else False
 sonka = True if input("Kell sonka? i/n") == "i" else False
 
 if (olivabogyo and pepperoni) and (pepperoni and not sonka):
-    print("Fincsike...")
-
+    print("Fincsike..."
+#ide is basztam tenni valkamitz
 
 else:
     print("miaszar...")
